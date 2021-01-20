83f81ebbd19d126ddcd8faf7b60296b8b1135fcd
Eye-Wuppertal <908095239@qq.com>
2021-01-20 22:34:27 +0800

    Day_01(1.20)

---
+++ day_01/Demo04_Const.java
@@ -0,0 +1,7 @@
+public class Demo04_Const{
+    public static void main(String[] args){
+        String str1 = "Hello";
+        String str2 = "World";
+        System.out.println(str1+str2+"!!!");//输出结果为“HelloWorld!!!”
+    }
+}
